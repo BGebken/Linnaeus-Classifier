19712f8aa228fa23b8bda95f3596b2fe553ec0fe
Bennett <35474536+BGebken@users.noreply.github.com>
2018-08-19 19:15:19 -0400

    Update LinnaeusClassifier.py
    updated to run the program as a loop that prompts the user for one input at a time and then prints results back to the screen

--- LinnaeusClassifier.py
+++ LinnaeusClassifier.py
@@ -6,35 +6,38 @@ import pandas as pd
 from sklearn.externals import joblib
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.externals import joblib
-
 # import the objects used by the vectorizer and classifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
-
 # load the classifier
 clf = joblib.load(filename='MasterModel.pkl')
 # load the vectorizer
 vectorizer = joblib.load(filename='vectorizer.pkl')
-
-#FIX THIS LINE - needs to read from a POST request
-# read the incoming data
-df = pd.read_csv(r'test.csv')
-
-# mark the raw data as "narrative"
-x_test = vectorizer.transform(df['NARRATIVE'])
-y_test_pred = clf.predict(x_test)
-# add the automatically generated results
-df.loc[:, ('Autocode')] = y_test_pred
-df.head(10)
-# Accessing the Predictions
-y_pred_prob = clf.predict_proba(x_test)
-max_index = y_pred_prob[0].argmax()
-code = clf.classes_[max_index]
-# get a sequence indicating the position with the highest probability for each row
-top_positions = y_pred_prob.argmax(axis=1)
-top_probabilities = y_pred_prob[np.arange(len(top_positions)), top_positions]
-df.loc[:, ('Probability')] = top_probabilities
-df.head()
-
-#FIX - need to update this so that it provides results back a returned value (integrate with API) 
-df.to_excel('test2_autocoded.xlsx')
+#initiate variables
+active = True
+df = pd.DataFrame(columns=['NARRATIVE'])
+output = 0
+while active:
+    # read the incoming data
+    contention = input('Enter your contention (type Q to quit): ')
+    if contention == "Q":
+        active = False
+    #read data
+    df = df.append({'NARRATIVE': contention}, ignore_index=True)
+    #Classify the contention
+    x_test = vectorizer.transform(df['NARRATIVE'])
+    y_test_pred = clf.predict(x_test)
+    df.loc[:, ('Autocode')] = y_test_pred
+    df.head(10)
+    y_pred_prob = clf.predict_proba(x_test)
+    max_index = y_pred_prob[0].argmax()
+    code = clf.classes_[max_index]
+    top_positions = y_pred_prob.argmax(axis=1)
+    top_probabilities = y_pred_prob[np.arange(len(top_positions)), top_positions]
+    df.loc[:, ('Probability')] = top_probabilities
+    df.head()
+    #Print Results
+    print(df.loc[[output]])
+    output += 1 #increase counter
+#dump a copy of the results for analysis
+df.to_excel('results.xlsx')
